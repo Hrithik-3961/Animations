053696cbef1f1f0dd694add2066c68b0876adc00
Hrithik Agarwal <agarwal.hrithik3961@gmail.com>
2021-08-18 15:17:31 +0530

    Implemented Object Animator

--- app/src/main/java/com/hrithik/animation/Activity1.kt
+++ app/src/main/java/com/hrithik/animation/Activity1.kt
@@ -1,19 +1,59 @@
 package com.hrithik.animation
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.hrithik.animation.databinding.Activity1Binding
 
 class Activity1 : AppCompatActivity() {
+
+    private lateinit var binding: Activity1Binding
+    private lateinit var objectAnimator1: ObjectAnimator
+    private lateinit var objectAnimator2: ObjectAnimator
+    private val animatorSet = AnimatorSet()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_1)
+        binding = Activity1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        objectAnimator1 = ObjectAnimator.ofFloat(binding.foreground1, "translationX", -210f)
+        objectAnimator2 = ObjectAnimator.ofFloat(binding.foreground2, "translationX", 210f)
+
+        animatorSet.apply {
+            playTogether(objectAnimator1, objectAnimator2)
+            childAnimations.forEach {
+                val t = it as ObjectAnimator
+                t.duration = 1200
+            }
+            addListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(p0: Animator?) {
+                }
+
+                override fun onAnimationEnd(p0: Animator?) {
+                    Handler(Looper.getMainLooper()).postDelayed( { animatorSet.start() }, 1200)
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {
+                }
+
+                override fun onAnimationRepeat(p0: Animator?) {
+                }
+            })
+        }
+
     }
 
-    fun openActivity2(view: View) {
-        val intent = Intent(this, Activity2::class.java)
-        startActivity(intent)
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+
+        animatorSet.start()
     }
 }
